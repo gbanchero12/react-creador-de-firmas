2b2b66efe6bf79b218b80e99437c23f3bcf44662
Guillermo Banchero <gbanchero12@gmail.com>
2020-03-16 16:41:36 -0300

    agregando eventos al formulario

--- src/components/BadeForm.js
+++ src/components/BadeForm.js
@@ -1,79 +1,104 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
+import '../components/styles/badge.css'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: 200,
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
     },
-  },
-  button:{
-    margin: theme.spacing(1),
-    width: 200,
-    height: 45,
-  }
+    button: {
+        margin: theme.spacing(1),
+        width: 200,
+        height: 45,
+        alignItemsAndJustifyContent: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+    }
 }));
 
-export default function FormPropsTextFields() {
-  const classes = useStyles();
 
-  return (
-    <div className={classes.root} >
-      
-     
-        <TextField
-        id="standard-full-width"
-        margin="normal"
-        name="nombre"
-        className="nombre"
-          label="Nombre"
-          defaultValue=""
-          variant="outlined"
-          
-        />
-        <TextField
-          name="apellido"
-          className="apellido"
-          label="Apellido"
-          defaultValue=""
-          variant="outlined"
-          
-        />
-         <TextField
-          name="posicion"
-          className="posicion"
-          label="Posicion"
-          defaultValue=""
-          variant="outlined"
-          
-        />
-         <TextField
-          name="telefono"
-          className="telefono"
-          label="Telefono"
-          defaultValue=""
-          variant="outlined"
-          
-        />
-          <TextField
-          name="email"
-          className="email"
-          label="Email"
-          defaultValue=""
-          variant="outlined"
-          
-        />
-       
-         <Button className={classes.button} variant="contained" color="primary" m={4}>
-        Guardar
+
+
+function FormPropsTextFields() {
+    const classes = useStyles();
+
+    const handleChange = (evento)=>{
+        console.log({value: evento.target.value});
+    }
+
+
+    return (
+        <div className="Badge">
+            <div className={classes.root} >
+
+                <h1>Nueva firma: </h1>
+                <TextField
+                    onChange={handleChange}
+                    margin="normal"
+                    name="nombre"
+                    className="nombre"
+                    label="Nombre"
+                    defaultValue=""
+                    variant="outlined"
+
+                />
+                <TextField
+                    onChange={handleChange}
+                    name="apellido"
+                    className="apellido"
+                    label="Apellido"
+                    defaultValue=""
+                    variant="outlined"
+
+                />
+                <TextField
+                    onChange={handleChange}
+                    name="posicion"
+                    className="posicion"
+                    label="Posicion"
+                    defaultValue=""
+                    variant="outlined"
+
+                />
+                <TextField
+                    onChange={handleChange}
+                    name="telefono"
+                    className="telefono"
+                    label="Telefono"
+                    defaultValue=""
+                    variant="outlined"
+
+                />
+                <TextField
+                    onChange={handleChange}
+                    name="email"
+                    className="email"
+                    label="Email"
+                    defaultValue=""
+                    variant="outlined"
+
+                />
+
+                <Grid item />
+                <Grid item />
+
+                <Button className={classes.button} variant="contained" color="primary">
+                    Guardar
       </Button>
 
-        
-        
-    
-    </div>
-  );
+
+
+            </div>
+        </div>
+    );
 }
+
+
+export default FormPropsTextFields;
